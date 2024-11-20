00cc4426e2e030a298407a4b7e8596eaa5925ee0
Kevin27182 <kevinmnard@gmail.com>
2024-11-19 21:21:43 -0600

    Added a little separator element for the toolbar

--- src/mapbuilder/gui/ToolbarPanel.java
+++ src/mapbuilder/gui/ToolbarPanel.java
@@ -3,14 +3,11 @@ package mapbuilder.gui;
 import mapbuilder.helpers.GridPanelListener;
 import mapbuilder.helpers.ToolbarButtonListener;
 import mapbuilder.helpers.ToolbarListener;
-import mapbuilder.items.EmptyButton;
 import mapbuilder.items.SwordButton;
 import mapbuilder.templates.ToolbarButton;
 import mapbuilder.tools.TrashButton;
-
 import javax.swing.*;
 import java.util.ArrayList;
-import java.util.stream.IntStream;
 
 public class ToolbarPanel extends JPanel implements ToolbarButtonListener, GridPanelListener {
 
@@ -18,6 +15,7 @@ public class ToolbarPanel extends JPanel implements ToolbarButtonListener, GridP
     private ArrayList<ToolbarButton> structures = new ArrayList<>();
     private ArrayList<ToolbarButton> items = new ArrayList<>();
     private ArrayList<ToolbarButton> creatures = new ArrayList<>();
+    private ArrayList<ToolbarButton> tools = new ArrayList<>();
     private ArrayList<ToolbarListener> listeners = new ArrayList<>();
     private ToolbarButton currentButton;
     private boolean selectionActive = false;
@@ -25,8 +23,7 @@ public class ToolbarPanel extends JPanel implements ToolbarButtonListener, GridP
     public ToolbarPanel() {
         setBackground(Theme.DARK_BACKGROUND_2);
         addButton(items, new SwordButton());
-        IntStream.range(0,19).forEach(_ -> addButton(items, new EmptyButton()));
-        addButton(items, new TrashButton());
+        addButton(tools, new TrashButton());
         renderButtons();
     }
 
@@ -35,6 +32,8 @@ public class ToolbarPanel extends JPanel implements ToolbarButtonListener, GridP
         structures.forEach(this::add);
         items.forEach(this::add);
         creatures.forEach(this::add);
+        add(new ToolbarSeparator());
+        tools.forEach(this::add);
     }
 
     public void addButton(ArrayList<ToolbarButton> list, ToolbarButton item) {
---
+++ src/mapbuilder/gui/ToolbarSeparator.java
@@ -0,0 +1,12 @@
+package mapbuilder.gui;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class ToolbarSeparator extends JPanel {
+
+    public ToolbarSeparator() {
+        setPreferredSize(new Dimension(2, Theme.TOOLBAR_BUTTON_HEIGHT));
+        setBackground(Theme.DARK_BACKGROUND_4);
+    }
+}
